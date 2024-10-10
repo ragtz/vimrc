0aeab5243c7bb028ee76858ec5404a9d2d976dfa
ragtz <ragtz@diligentrobots.com>
2024-10-10 18:39:28 -0500

    Add command for python shebag

--- NvChad/lua/custom/init.lua
+++ NvChad/lua/custom/init.lua
@@ -1,3 +1,4 @@
 vim.opt.colorcolumn = "120"
 
-vim.api.nvim_create_user_command('DCR', ':0r ~/diligent_copyright_python.txt', {})
+vim.api.nvim_create_user_command('DCR', ':0r ~/.config/nvim/lua/custom/templates/diligent_copyright_python.txt', {})
+vim.api.nvim_create_user_command('PYSB', ':0r ~/.config/nvim/lua/custom/templates/python_shebang.txt', {})
